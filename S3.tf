c55134665804655c2c733224246d14e7bfb092c3
chaitu <chaituchaithanya200@gmail.com>
2025-11-08 12:11:07 +0530

    newcommits

--- S3.tf
+++ S3.tf
@@ -1,2 +1,30 @@
-I
+provider "aws" {
+  region = "us-east-1"
+}
 
+resource "aws_s3_bucket" "one" {
+  bucket = "terraform.chaithu.one"
+}
+
+resource "aws_s3_bucket_versioning" "two" {
+  bucket = aws_s3_bucket.one.id
+  versioning_configuration  {
+    status = "Enabled"
+  }
+}
+
+
+
+# IAM POLICES CREATE USER S3 FULL PERMISSIONS FOR EC2 or else error regarding permissions
+
+# for multiple buckets with different names at a time
+
+
+resource "aws_s3_bucket" "os" {
+  for_each = toset(var.abc)
+  bucket = each.key
+}
+
+variable "abc" {
+  default = ["jenkins.flm.bucket" , "docker.flm.bucket", "k8s.flm.bucket"] #diffrent bucket names
+}
